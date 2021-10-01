ed263e44d10dd0dba760724d2cad59e827d2693b
Raileny <raileny44@gmail.com>
2021-09-30 22:18:01 -0300

    tela de inicio

---
+++ Feed/Scenes/Login/SignIn/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  Feed
+//
+//  Created by Natália Carolina Dos Santos on 30/09/21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
