85b65122e34083d210562417a4f2beecc2ec3a35
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-09-30 21:21:47 -0300

    pod firebase instalado

--- Feed/Sources/AppDelegate.swift
+++ Feed/Sources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        FirebaseApp.configure()
+        
         let rootViewController = UIViewController()
         
         rootViewController.view.backgroundColor = .blue
@@ -21,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
+        
         
                 return true
     }
