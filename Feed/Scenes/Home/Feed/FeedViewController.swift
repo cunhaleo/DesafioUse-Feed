2ceb4ca2e278c5536b8f75f04af61749b64f19fb
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-10-01 19:50:02 -0300

    criação tableView e tableViewCell do Feed

---
+++ Feed/Scenes/Home/Feed/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Feed
+//
+//  Created by Leonardo Cunha on 01/10/21.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+
+
+}
