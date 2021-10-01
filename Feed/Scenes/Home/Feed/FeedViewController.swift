f61cd7b6f459014b937e020112969669fba02b03
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-10-01 20:44:46 -0300

    construtores da tableView e xib da cell

--- Feed/Scenes/Home/Feed/FeedViewController.swift
+++ Feed/Scenes/Home/Feed/FeedViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTableView()
    
     }
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        tableView.register(UINib(nibName: "FeedTableViewCell", bundle: nil), forCellReuseIdentifier: "FeedTableViewCell")
+        tableView.reloadData()
+    }
 
+}
 
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell") as? FeedTableViewCell {
+          return cell
+        }else{
+            return UITableViewCell()
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
