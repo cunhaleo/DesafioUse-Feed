425fc544632866ea2ac7320dddcf497bd5707d61
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-10-01 21:16:35 -0300

    configurando celula da tableview

--- Feed/Scenes/Home/Feed/CellFeed/FeedTableViewCell.swift
+++ Feed/Scenes/Home/Feed/CellFeed/FeedTableViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var labelPost: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
